d0da62070e67f1cda5d57c2305466c6eb4894aaa
Chrislandry98 <takamlandry1998@gmail.com>
2025-06-07 12:12:42 -0400

    Modification du header et du footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+/*variable pour cacher les bouton whatsapp et telegram*/
+  estCache: boolean = false;
+
+  /*Fonction pour gerer l'affichage des boutons reseaux sociaux*/
+  hiddeButton(){
+    this.estCache = !this.estCache;
+    console.log(this.estCache);
+  }
 }
